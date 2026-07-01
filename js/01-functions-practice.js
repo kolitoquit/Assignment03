91efe338ccdd8d14ee13e0e618df6ca0ffe79493
Krizia Olitoquit <kriziaolitoquit@gmail.com>
2026-06-30 21:31:22 -0700

    functions practice

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,14 +1,32 @@
 //STEP 1
-function halfNumber(num) {
+/* function halfNumber(num) {
     const result = num / 2;
     alert(`Half of ${num} is ${result}.`);
 }
 halfNumber(5);
+ */
 
 //STEP 2
+/*function squareNumber(num) {
+    const result = num * num;
+    alert(`The result of squaring the number ${num} is ${result}.`);
+}
+squareNumber(3);
+*/
 
 //STEP 3
+/* function percentOf(num1, num2) {
+    const result = (num1 / num2) * 100;
+    alert(`${num1} is ${result}% of ${num2}.`);
+}
+percentOf(2, 4);
+*/
 
 //STEP 4
+/* function findModulus(num1, num2) {
+    const result = num1 % num2;
+    alert(`${result} is the modulus of ${num2} and ${num2}.`);
+}
 
-//STEP 5
+findModulus(10, 4);
+*/
