72df1dfc5c6b5ac12d865f815bef5264a394cd62
Krizia Olitoquit <kriziaolitoquit@gmail.com>
2026-06-30 21:54:35 -0700

    calculator

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,9 +1,25 @@
 // ADD A FUNCTION CALLED CALCULATE
+function calculate(num1, num2, operation) {
+    if (operation === "+") {
+        return num1 + num2;
+    } else if (operation === "-") {
+        return num1 - num2;
+    } else if (operation === "*") {
+        return num1 * num2;
+    } else if (operation === "/") {
+        return num1 / num2;
+    }
+}
 
 // COLLECT FIRST NUMBER FROM USER
+const num1 = parseFloat(prompt("Enter the first number:"));
 
 // COLLECT SECOND NUMBER FROM USER
+const num2 = parseFloat(prompt("Enter the second number:"));
 
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+const operation = prompt("Enter the operation to perform (+, -, *, /):");   
 
 // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+const result = calculate(num1, num2, operation);
+alert(`The result of ${num1} ${operation} ${num2} is: ${result}`);
